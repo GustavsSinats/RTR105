954bad1d20a9e55730338a13270191b3bb2fdf1c
GustavsSinats <144107004+GustavsSinats@users.noreply.github.com>
2023-10-16 21:10:55 +0300

    Add files via upload

---
+++ dec2bin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char num;
+    
+    // Prasa lietotajam ievadit naturalu skaitli
+    printf("Ievadi naturalu skaitli no -128 lidz 127: ");
+    scanf("%hhd", &num);
+    
+    // Sagatavo un izprinte binaro skaitli izmantojot bitu parbidi
+    printf("Skaitlis binaraja koda: ");
+    for (int i = 7; i >= 0; i--) {
+        // Izpilda parbidi pa labi un izmanto bitu parbidi "AND", lai bits atrostos pozicija i
+        char bit = (num >> i) & 1;
+        printf("%d", bit);
+    }
+    
+    printf("\n");
+    return 0;
+}
+
