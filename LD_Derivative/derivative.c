3928de98efc4ba1b26a61b132e5b601c7ba70825
GustavsSinats <144107004+GustavsSinats@users.noreply.github.com>
2023-12-11 23:20:46 +0200

    Update derivative.c

--- LD_Derivative/derivative.c
+++ LD_Derivative/derivative.c
@@ -4,33 +4,25 @@
 #include<math.h>
 
 void main (){
-    int a;
-    int b;
-    int precizitate;
+    FILE *pFile;
+   float a, b, precizitate;
+    int k;
 
     printf("Lūdzu ievadi a vērtību: \n");
-    scanf("%d",&a);
+    scanf("%f",&a);
     printf("Lūdzu ievadi b vērtibu: \n");
-    scanf("%d",&b);
+    scanf("%f",&b);
     printf("Lūdzu ievadi cik precīzu rezultātu tu vēlies (cik ciparus aiz komata): \n");
-    scanf("%d",&precizitate);
+    scanf("%f",&precizitate);
     
-    FILE *pFile;
+    
     pFile = fopen ("derivative.dat","w");
-    float a =("%d",a).,b=("%d",b)*M_PI,x,delta_x=1.e-2;
+    fprintf((pFile, "asin(x)analytical'forward difference'analytical''forward difference'' \n");)
     //float y;
 
-    pFile = fopen ("derivative.dat","w");
-    fprintf(pFile,"\tx\t\tsin(x)\t\tsin\'(x)\n");
+    int i = ((b-a)/precizitate);
+   
     
-    x = a;
-    while (x < b)
-    {
-        // y=sin(x);
-        fprintf(pFile, "%10.2f\t%10.2f\t%10.2f\n", x, asin(x), (asin(x + delta_x) - asin(x)) / delta_x);
-        // printf(”%10.2f\t%10.2f\n”,x,y);
-        x += delta_x; // x=x+delta_x;
-    }
-    fclose(pFile);
+    
     return 0;
 }
